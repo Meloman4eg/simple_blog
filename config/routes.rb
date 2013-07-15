402555ad62b0a3c8f06bd41d543563f0ddfae876
Pavel Dadykin <pavel.splean@gmail.com>
2013-07-15 15:38:49 +0200

    now you can add posts

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Blog::Application.routes.draw do
   get "welcome/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+  resources :posts
   # You can have the root of your site routed with "root"
   root 'welcome#index'
 
@@ -40,7 +40,7 @@ Blog::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
